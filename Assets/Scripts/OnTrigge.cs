efacf029c90b2cb680e46b16777ecee6a57aa259
ttraintimes <ttraintimes@gmail.com>
2021-09-04 21:00:14 +0800

    scripts

---
+++ Assets/Scripts/OnTrigge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigge : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<GameManamge>().a2 = 1;
+        gameObject.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/OnTrigge1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigge1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<GameManamge>().a2 = 2;
+        gameObject.SetActive(false);
+    }
+}
